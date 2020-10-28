a2920f4a48b97af7b9adc1019834b3990664594d
Hashim Gari <hashmaster3k@gmail.com>
2020-10-28 16:30:42 -0600

    Index endpoint for user

---
+++ spec/requests/api/v1/users_request_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'USER API' do
+  it 'sends a list of users' do
+    create_list(:user, 5)
+
+    get '/api/v1/users'
+
+    expect(response).to be_successful
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    users = json[:data]
+
+    expect(users).to be_an(Array)
+    expect(users.count).to eq(5)
+
+    users.each do |user|
+      expect(user).to have_key(:id)
+      expect(user[:id]).to be_an(String)
+      expect(user).to have_key(:type)
+      expect(user[:type]).to be_an(String)
+      expect(user).to have_key(:attributes)
+      expect(user[:attributes]).to be_a(Hash)
+      expect(user[:attributes]).to have_key(:id)
+      expect(user[:attributes][:id]).to be_an(Integer)
+      expect(user[:attributes]).to have_key(:email)
+      expect(user[:attributes][:email]).to be_an(String)
+    end
+  end
+end
