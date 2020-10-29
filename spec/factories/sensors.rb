e5cbdb2c6fdd7ffd56cde9eebd02d937423e3040
Logan Riffell <lkriffell@gmail.com>
2020-10-28 20:13:05 -0600

    Add factories for gardens, sensors, user_gardens and users

--- spec/factories/sensors.rb
+++ spec/factories/sensors.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :sensor do
-    
+    min_threshold { Faker::Number.between(from: 1, to: 10) }
+    max_threshold { Faker::Number.between(from: 10, to: 20) }
   end
+
+    trait :moisture_sensor do
+      sensor_type { 0 }
+    end
+
+    trait :light_sensor do
+      sensor_type { 1 }
+    end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
-    
+    email { Faker::Internet.email }
+    provider { "Google" }
+    token { "jdaklhgu849urfonu9wr" }
   end
 end
