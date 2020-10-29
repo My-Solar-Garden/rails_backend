ebbbdcc4ea78c7ad74e0f4fa69aadd8c163ed037
Logan Riffell <lkriffell@gmail.com>
2020-10-29 12:09:36 -0600

    Refactor routes to use resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      # Sensors
-      get '/users/:id/gardens/:id/sensors', to: 'sensors#index'
-      get '/users/:id/gardens/:id/sensors/:id', to: 'sensors#show'
-      post '/users/:id/gardens/:id/sensors', to: 'sensors#create'
-      patch '/users/:id/gardens/:id/sensors/:id', to: 'sensors#update'
-      delete '/users/:id/gardens/:id/sensors/:id', to: 'sensors#delete'
+      resources :users do
+        resources :gardens do
+          # Sensors
+          get '/sensors', to: 'sensors#index'
+          get '/sensors/:id', to: 'sensors#show'
+          post '/sensors', to: 'sensors#create'
+          patch '/sensors/:id', to: 'sensors#update'
+          delete '/sensors/:id', to: 'sensors#delete'
+        end
+      end
     end
   end
 end
