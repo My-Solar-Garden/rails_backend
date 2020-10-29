fd34685d1b7b81989ff5b6ddf1d021bc1c24089a
Daniel Lessenden <dless27@gmail.com>
2020-10-28 20:18:37 -0500

    Create show action for the garden controller and the accompanying test

--- app/controllers/api/v1/gardens_controller.rb
+++ app/controllers/api/v1/gardens_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::GardensController < ApplicationController
   def index
     render json: GardenSerializer.new(Garden.all)
   end
+
+  def show
+    render json: GardenSerializer.new(Garden.find(params[:id]))
+  end
 end
--- spec/requests/garden_request_spec.rb
+++ spec/requests/garden_request_spec.rb
@@ -31,5 +31,35 @@ describe 'garden API' do
         expect(garden[:attributes][:name]).to be_an(String)
       end
     end
+
+      it 'can return a garden by its id' do
+        new_garden = create(:garden)
+        get "/api/v1/gardens/#{new_garden.id}"
+        expect(response).to be_successful
+
+        garden = JSON.parse(response.body, symbolize_names: true)[:data]
+
+        expect(garden).to have_key(:id)
+        expect(garden[:id]).to be_an(String)
+
+        expect(garden).to have_key(:type)
+        expect(garden[:type]).to be_an(String)
+
+        expect(garden).to have_key(:attributes)
+        expect(garden[:attributes]).to be_a(Hash)
+
+        expect(garden[:attributes]).to have_key(:latitude)
+        expect(garden[:attributes][:latitude]).to be_an(Float)
+
+        expect(garden[:attributes]).to have_key(:longitude)
+        expect(garden[:attributes][:longitude]).to be_an(Float)
+
+        expect(garden[:attributes]).to have_key(:name)
+        expect(garden[:attributes][:name]).to be_an(String)
+
+        expect(garden[:attributes][:latitude]).to eq(new_garden.latitude)
+        expect(garden[:attributes][:longitude]).to eq(new_garden.longitude)
+        expect(garden[:attributes][:name]).to eq(new_garden.name)
+      end
   end
 end
