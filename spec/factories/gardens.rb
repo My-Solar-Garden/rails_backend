376fb804b3b951836fafbb1dede8fb184d05837d
Angela Guardia <angelaguardia20@gmail.com>
2020-10-29 10:25:36 -0700

    Update spec/factories/gardens.rb

--- spec/factories/gardens.rb
+++ spec/factories/gardens.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :garden do
     latitude { Faker::Address.latitude }
     longitude { Faker::Address.longitude }
-    description { "This is a description" }
     name { Faker::FunnyName.name }
   end
 end
