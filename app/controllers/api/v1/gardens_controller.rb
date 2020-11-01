c56b638195fba9127dc711d31eb0e8ea0340671c
Leah Riffell <leah.riffell@gmail.com>
2020-11-01 15:14:06 -0700

    Fix add new garden controller action

--- app/controllers/api/v1/gardens_controller.rb
+++ app/controllers/api/v1/gardens_controller.rb
@@ -11,7 +11,7 @@ class Api::V1::GardensController < ApplicationController
 
   def create
     user = User.find(params[:user_id])
-    new_garden = user.gardens.new(garden_params)
+    new_garden = user.gardens.create(garden_params)
     render json: GardenSerializer.new(new_garden) if new_garden.save
   end
 
