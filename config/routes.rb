53a2e30f047e1f06b6e6c8ba7d63648cc91a6cdb
Nico Rithner <nicorithner@gmail.com>
2020-11-04 16:35:18 -0700

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      get '/forecast', to: 'weather#show'
       get '/garden_healths/search', to: 'search#index'
       namespace :plants do
         get '/search', to: 'search#index'
