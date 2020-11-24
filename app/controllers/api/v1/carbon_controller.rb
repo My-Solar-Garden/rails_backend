5212e6e71c67546265b82ebe43919dbac7060989
Dani Coleman <danicoleman00@gmail.com>
2020-11-24 13:15:36 -0600

    Add method to get json payload and format with the actual data that we want to receive back. Not refactored yet.

---
+++ app/controllers/api/v1/carbon_controller.rb
@@ -0,0 +1,31 @@
+class Api::V1::CarbonController < ApplicationController
+  def search
+    # https://plant-carbon-api.herokuapp.com/api/v1/search?q=okra
+    conn = Faraday.new('https://plant-carbon-api.herokuapp.com')
+
+    result = conn.get('/api/v1/search') do |req|
+      req.params[:q] = params[:plant]
+    end
+    result = JSON.parse(result.body, symbolize_names: true)
+
+    output = result[:data].map do |plant|
+      attr = plant[:attributes]
+      {
+        attributes: {
+          name: attr[:name],
+          latin: attr[:latin],
+          average_exchange_percentage: attr[:arith_mean],
+          standard_deviation_error: attr[:stderror],
+          research_journals: attr[:journals]
+        }
+      }
+    end
+
+    response = {
+      data: output
+    }
+
+    render json: response
+  end
+
+end
