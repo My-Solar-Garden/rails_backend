70d5da84cc88187f4ba3338fb3b25975204f429d
Dani Coleman <danicoleman00@gmail.com>
2020-11-24 18:21:17 -0600

    Comment out code as it moves to service

--- app/controllers/api/v1/carbon_controller.rb
+++ app/controllers/api/v1/carbon_controller.rb
@@ -2,24 +2,25 @@ class Api::V1::CarbonController < ApplicationController
   def search
     # https://plant-carbon-api.herokuapp.com/api/v1/search?q=okra
     conn = Faraday.new('https://plant-carbon-api.herokuapp.com')
-
-    result = conn.get('/api/v1/search') do |req|
-      req.params[:q] = params[:plant]
-    end
-    result = JSON.parse(result.body, symbolize_names: true)
-
-    output = result[:data].map do |plant|
-      attr = plant[:attributes]
-      {
-        attributes: {
-          name: attr[:name],
-          latin: attr[:latin],
-          average_exchange_percentage: attr[:arith_mean],
-          standard_deviation_error: attr[:stderror],
-          research_journals: attr[:journals]
-        }
-      }
-    end
+    
+    result = CarbonService.search(params[:plant])
+    # result = conn.get('/api/v1/search') do |req|
+    #   req.params[:q] = params[:plant]
+    # end
+    # result = JSON.parse(result.body, symbolize_names: true)
+    #
+    # output = result[:data].map do |plant|
+    #   attr = plant[:attributes]
+    #   {
+    #     attributes: {
+    #       name: attr[:name],
+    #       latin: attr[:latin],
+    #       average_exchange_percentage: attr[:arith_mean],
+    #       standard_deviation_error: attr[:stderror],
+    #       research_journals: attr[:journals]
+    #     }
+    #   }
+    # end
 
     response = {
       data: output
@@ -27,5 +28,4 @@ class Api::V1::CarbonController < ApplicationController
 
     render json: response
   end
-
 end
