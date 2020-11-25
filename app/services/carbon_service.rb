8bb1bb124d9572de10c3402d6c93a0b576c402b1
Dani Coleman <danicoleman00@gmail.com>
2020-11-24 18:22:06 -0600

    Add method that returns data payload in a serialized fashion

--- app/services/carbon_service.rb
+++ app/services/carbon_service.rb
@@ -3,7 +3,25 @@ class CarbonService
     response = conn.get('/api/v1/search') do |f|
       f.params[:q] = search_term
     end
-    json = JSON.parse(response.body, symbolize_names: true)
+
+    result = JSON.parse(response.body, symbolize_names: true)
+    require "pry"; binding.pry
+    output = result[:data].map do |plant|
+      attr = plant[:attributes]
+      {
+        attributes: {
+          name: attr[:name],
+          latin: attr[:latin],
+          average_exchange_percentage: attr[:arith_mean],
+          standard_deviation_error: attr[:stderror],
+          research_journals: attr[:journals]
+        }
+      }
+    end
+    require "pry"; binding.pry
+    output
+    # # json = JSON.parse(response.body, symbolize_names: true)
+    # json = JSON.parse(output.body, symbolize_names: true)
   end
 
   def self.conn
